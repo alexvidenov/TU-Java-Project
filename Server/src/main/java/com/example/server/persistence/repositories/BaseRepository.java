cc21feaa9a3d455cb6c918f0c531f60b9e2fee5f
Kiril Chingarov <kiril.chingarov@gmail.com>
2024-01-11 00:24:23 +0200

    Removed Base repository and service

--- Server/src/main/java/com/example/server/persistence/repositories/BaseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.server.persistence.repositories;
-
-import com.example.server.persistence.entities.BaseEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BaseRepository extends JpaRepository<BaseEntity, Long> {
-    BaseEntity GetBaseEntityById(Long id);
-}
